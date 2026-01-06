cdd2366888046f0c7b255e0c41c033101793d598
Keshav334736 <keshavrauniyar78@gmail.com>
2026-01-06 17:59:23 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,28 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a=9;
+        int b=3;
+        int add;
+        int difference;
+        int Multiplication;
+        int division;
+        int modulus;
+        //Addition
+         add=a+b;
+         //difference
+         difference=a-b;
+         //Multiplication
+         Multiplication=a*b;
+         //Division
+         division=a/b;
+         //modulus
+         modulus=a%b;
+
+         System.out.println(add);
+         System.out.println(difference);
+         System.out.println(Multiplication);
+         System.out.println(division);
+         System.out.println(modulus);
+    }
+    
+}
