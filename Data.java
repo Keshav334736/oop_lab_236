ff3f80ab7bd0b617361b90771bbe7b1f16f88805
Keshav334736 <keshavrauniyar78@gmail.com>
2026-01-06 17:46:14 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,22 @@
+public class Data{
+    public static void main(String[] args) {
+        int age=19;
+        String name="keshav";
+        double a=15.5;
+        float d=25.6f;
+        byte kk=121;
+        short b=(short)11.987;
+        long e=98727737237222L;
+        char c='K';
+        boolean isstudent=true;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(kk);
+        System.out.println(b);
+        System.out.println(e);
+        System.out.println(c);
+    }
+}
